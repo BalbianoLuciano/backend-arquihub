dfc953b49a33c19966e86047269f3851b8b791a9
Gonzalo <102692082+gnz6@users.noreply.github.com>
2022-09-28 16:16:33 -0300

    controller post con validaciones

--- controllers/post.js
+++ controllers/post.js
@@ -2,37 +2,93 @@ const { postModel } = require("../models")
 
 const getPosts = async (req, res) => {
     try {
-        console.log("hola")
         const allPosts = await postModel.findAllData({})
         res.send(allPosts)
 
     } catch (error) {
-        console.log(error)
+        res.status(400).send("No posts found")
     }
 }
 
 const createPost = async (req, res) => {
     try {
-        const { body } = req;
-        const newPost = await postModel.create(body)
+        const { title, 
+            description, 
+            visibility, 
+            createdBy, 
+            project_type, 
+            mts2,
+            rooms,
+            year,
+            bathrooms,
+            authors,
+            additional_data,
+            rating
+        } = req.body;
+
+    if(!title|| !description || !createdBy || !project_type ){
+        return res.status(400).send("Missing required parameters")
+    }
+    
+        const newPost = {
+            title, 
+            description, 
+            visibility, 
+            createdBy, 
+            project_type, 
+            mts2,
+            rooms,
+            year,
+            bathrooms,
+            authors,
+            additional_data,
+            rating
+        }
+        console.log(newPost)
+        await postModel.create(newPost)
         res.send(newPost)
 
     } catch (error) {
-        console.log(error)
+        res.status(400).send("Cant post this project")
     }
 }
 
 const updatePost = async (req, res) => {
     try {
         const { id } = req.params;
-        const { body } = req;
-        console.log(body)
-        const newPost = body
-        await postModel.findOneAndUpdate(id,  body )
-        res.send(newPost)
+        const { 
+            title, 
+            description, 
+            visibility, 
+            createdBy, 
+            project_type, 
+            mts2,
+            rooms,
+            year,
+            bathrooms,
+            authors,
+            additional_data,
+            rating } = req.body;
+
+        const updatePost = { 
+            title, 
+            description, 
+            visibility, 
+            createdBy, 
+            project_type, 
+            mts2,
+            rooms,
+            year,
+            bathrooms,
+            authors,
+            additional_data,
+            rating}
+
+        await postModel.findOneAndUpdate(id,  updatePost )
+        res.send(updatePost)
 
     } catch (error) {
-        console.log(error)
+        res.status(400).send("Cant update this post")
     }
 }
 
@@ -42,7 +98,7 @@ const deletePost = async (req, res) => {
        await postModel.deleteOne({_id:id})
         res.send("Post deleted")
     } catch (error) {
-        console.log(error)
+        res.status(400).send("Cant delete this post")
     }
 }
 

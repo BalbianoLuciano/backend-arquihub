723d1758d2dccfce947ee2bc69f67cccccd08878
Sebastian Diaz <sediaz84@gmail.com>
2022-09-29 11:07:01 -0300

    ruta update terminada

--- controllers/update.js
+++ controllers/update.js
@@ -1,62 +1,93 @@
 const { updateModel } = require("../models")
-const { projectModel } = require("../models")
+
 
 const getUpdates = async (req, res) => {
     try {
-        const allUpdate = await updateModel.find({})
+        const allUpdate = await updateModel.findAllData({})
         res.send(allUpdate)
 
     } catch (error) {
-        console.log(error)
+        res.status(400).send("No updates found")
     }
 }
 
-const postUpdate = async (req, res) => { //llegan title, description por body 
-    try {  
-        const { id } = req.params;      
-        const { body } = req;
-        //await projectModel.findById(id)
-        const newUpdate = await updateModel.create(body)
-        res.send(newUpdate)
+const createUpdate = async (req, res) => { //llegan title, description por body 
+    try { 
+        const { title, comments, project_id, user_id, storage_id } = req.body;
 
+        if(!title || !comments || !project_id || !user_id || !storage_id){
+            res.status(400).send("Missing fields to complete")
+        } else {
+        const newUpdate = { title, comments, project_id, user_id, storage_id}
+        await updateModel.create(newUpdate)
+        res.status(200).send(newUpdate)
+        }
     } catch (error) {
-        console.log(error)
+        res.status(400).send("Can't post this update")
     }
 }
 
-const putUpdate = async (req, res) => { //llegan projectId por params y title y description por body
+const putUpdate = async (req, res) => { 
     try {
         const { id } = req.params;
-        const { title, comments } = req.body;
-        //console.log(body)
-        await updateModel.findByIdAndUpdate(id, {title, comments})
-        res.send("File Update")
+        const { comments } = req.body;
 
+        if(!comments){
+            res.status(400).send("A comment is needed")
+        } else {
+        const update = { comments }
+        await updateModel.findByIdAndUpdate(id, update)
+        res.send(update)
+        }
     } catch (error) {
-        console.log(error)
+        res.status(400).send("Can't update")
     }
 }
 
-// const deletePost = async (req, res) => {
-//     try {
-//         const { id } = req.params;
-//        await postModel.deleteOne({_id:id})
-//         res.send("Post deleted")
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
 
 
-// const getPost = async (req, res) => {
-//     try {
-//         const { id } = req.params;
-//         const post = await postModel.findOne({_id:id})
-//         res.send(post)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+module.exports = { getUpdates, createUpdate, putUpdate }
 
+/*
+project
+{
+"_id": "633384d2925d0057f3f1745e",
+        "title": "Nisman",
+        "description": "adios giles",
+        "created_by": "6333a1b9118df73d073826f3",
+        "project_file": "aquí va el archivo",
+        "project_type": "casa",
+        "createdAt": "2022-09-27T23:18:42.126Z",
+        "updatedAt": "2022-09-28T23:24:32.647Z",
+        "__v": 0,
+        "users": [],
+        "created_by_data": []
+}
 
-module.exports = { getUpdates, postUpdate, putUpdate }
+user 
+{       
+"_id": "6334e691a6d62b6d071ec3e0",
+        "name": "Tom",
+        "lastname": "Menem",
+        "nickname": "Jvmpers",
+        "email": "peronista@kk.com",
+        "password": "asdasdasd",
+        "type": "user",
+        "projects": [],
+        "posts": [],
+        "favourites": [],
+        "status": "active",
+        "createdAt": "2022-09-29T00:28:01.244Z",
+        "updatedAt": "2022-09-29T00:28:01.244Z",
+        "__v": 0
+    },
+
+storage
+{
+    "_id": "633482bc35d8091929fc3244",
+    "filename": "file-1664385724132.dwg",
+    "createdAt": "2022-09-28T17:22:04.145Z",
+    "updatedAt": "2022-09-28T17:22:04.145Z",
+    "__v": 0
+},
+*/
--- models/storage.js
+++ models/storage.js
@@ -14,4 +14,18 @@ const StorageSchema = new mongoose.Schema(
   }
 );
 
+// StorageSchema.statics.findAllData = function(){
+//   const joinStorages = this.aggregate([
+//     {
+//         $lookup: {
+//           from: "updates",
+//           localField:"updates_id",
+//           foreignField: "_id",
+//           as: "updates"
+//         }
+//     }
+//   ])
+//   return joinStorages
+// }
+
 module.exports = mongoose.model("storages", StorageSchema);

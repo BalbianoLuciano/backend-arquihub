9b813dae193249b2f3e4a6e130ac31585eb4d5fe
BalbianoLuciano <balbiano06@gmail.com>
2022-09-29 16:12:45 -0300

    fixed I hope so

---
+++ models/Storage.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete")
+
+const StorageSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+    },
+    filename: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+StorageSchema.plugin(mongooseDelete, {overrideMethods: "all"})
+
+// StorageSchema.statics.findAllData = function(){
+//   const joinStorages = this.aggregate([
+//     {
+//         $lookup: {
+//           from: "updates",
+//           localField:"updates_id",
+//           foreignField: "_id",
+//           as: "updates"
+//         }
+//     }
+//   ])
+//   return joinStorages
+// }
+
+module.exports = mongoose.model("storages", StorageSchema);

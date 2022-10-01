966f7da7432cbcd69ff62b0dff198fb83f685cf0
Jvmpers <tomaspastore200111@gmail.com>
2022-10-01 19:51:33 -0300

    arreglado de post authors y favourites

--- controllers/favourites.js
+++ controllers/favourites.js
@@ -11,7 +11,7 @@ const updateFavourites = async (req, res) => {
   const favouriteRepeat = user.favourites.find( e=>  e.equals(user_id))?true:false
       if(favouriteRepeat)return res.status(404).send({err:"the post to add to favourite is repeated"})
       await usersModel.updateOne({_id:user_id},
-        { $push: { favourites_post: id } },
+        { $push: { favourites: id } },
         { new: true, useFindAndModify: false }
       );
     res.status(200).send({success:"post successfully added to favourites"});
@@ -31,7 +31,7 @@ const deleteFavourites = async (req, res) => {
     await usersModel.findOne({_id:user_id});
     await postModel.findOne({_id:id});
     await usersModel.updateOne({_id:user_id},
-      { $pull: { favourites_post: id } },
+      { $pull: { favourites: id } },
       { new: true, useFindAndModify: false }
     );
     res.status(200).send({success:"user successfully deleted to the project"});

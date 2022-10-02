96aa880bb1e0c1f6ca9aae91b32092e1e5312afe
Jvmpers <tomaspastore200111@gmail.com>
2022-10-02 18:43:35 -0300

    correccion mostrar authores

--- controllers/post.js
+++ controllers/post.js
@@ -152,7 +152,7 @@ const getPost = async (req, res) => {
                 },
               },]);
               const reviews = postReviews.filter(e=>e.post_id==id)
-            const userPost = await postModel.populate(post, {path:"users"}); 
+            const userPost = await postModel.populate(post, {path:"authors"}); 
            const getPost ={...userPost,reviews:reviews} 
             res.status(200).send(getPost);
     } catch (error) {

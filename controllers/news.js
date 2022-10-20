fd91bcf962872f484973cc03981d064d338434d9
Jvmpers <tomaspastore200111@gmail.com>
2022-10-20 02:49:00 -0300

    magia

--- controllers/news.js
+++ controllers/news.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const newsData = async (req, res) => {
     try{
       const news = await axios.get(
-      "https://newsapi.org/v2/everything?domains=archdaily.com&apiKey=761b6e1111a74c789561b3cbaa3f9515"
+      "https://newsapi.org/v2/everything?domains=archdaily.com&apiKey=88f3c30c0a6d497caefe03fbc8835646"
     );
     const filterNews = news.data.articles.map((e, index) => {
       return {
